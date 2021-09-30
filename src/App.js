d32171403ec1723d5cfcbcddce892736ffc1fea9
ramirez <prlara2000@gmail.com>
2021-09-30 16:25:14 -0500

    add new expense component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Expenses from "./components/Expenses";
-
+import NewExpense from "./components/NewExpense";
 function App() {
   return (
     <div id="app">
+      <NewExpense />
       <section>
         <Expenses />
       </section>
---
+++ src/components/NewExpense.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NewExpense.css";
+function NewExpense() {
+  return <div className="div">hello</div>;
+}
+
+export default NewExpense;
